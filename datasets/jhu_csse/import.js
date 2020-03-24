33abe612d86bdd44bf3d742b4cab5adefd949567
Thomas Rueckstiess <thomas.rueckstiess@mongodb.com>
2020-03-24 18:33:43 +1100

    Switch to new file format (removing recovery data). Prettier enabled.

--- datasets/jhu_csse/import.js
+++ datasets/jhu_csse/import.js
@@ -2,7 +2,7 @@ const _ = require("lodash");
 const csv = require("csvtojson");
 const MongoClient = require("mongodb").MongoClient;
 
-const password = process.env['COVID_CLUSTER_PW'];
+const password = process.env["COVID_CLUSTER_PW"];
 
 const CONFIRMED_FILE =
   "COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
@@ -10,24 +10,19 @@ const DEAD_FILE =
   "COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv";
 const RECOVERED_FILE =
   "COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv";
-const MONGO_URI =
-  `mongodb+srv://admin:${password}@coronacluster-eydl1.mongodb.net/test?retryWrites=true&w=majority`;
+const MONGO_URI = `mongodb+srv://admin:${password}@coronacluster-eydl1.mongodb.net/test?retryWrites=true&w=majority`;
 const DATABASE_NAME = "covid19";
 const COLLECTION_NAME = "jhu_csse";
 
 // Promises to import CSV files
-const promises = [
-  csv().fromFile(CONFIRMED_FILE),
-  csv().fromFile(DEAD_FILE),
-  csv().fromFile(RECOVERED_FILE)
-];
+const promises = [csv().fromFile(CONFIRMED_FILE), csv().fromFile(DEAD_FILE)];
 
 Promise.all(promises)
   .then(tables => {
     const docsToInsert = [];
     const importDate = new Date();
 
-    const [confirmed, dead, recovered] = tables;
+    const [confirmed, dead] = tables;
     const headers = _.keys(confirmed[0]);
     const firstDateColumn = _.findIndex(headers, s => s.startsWith("1"));
 
@@ -36,7 +31,6 @@ Promise.all(promises)
       // get rows from the tables
       const confirmedRow = confirmed[rowIdx];
       const deadRow = dead[rowIdx];
-      const recoveredRow = recovered[rowIdx];
 
       // build base object without dates
       const baseObjectKeys = headers.slice(0, firstDateColumn);
@@ -52,7 +46,6 @@ Promise.all(promises)
         docToInsert.date = new Date(`${date} UTC`);
         docToInsert.confirmed = parseInt(confirmedRow[date], 10) || 0;
         docToInsert.dead = parseInt(deadRow[date], 10) || 0;
-        docToInsert.recovered = parseInt(recoveredRow[date], 10) || 0;
         docToInsert.importDate = importDate;
         // push doc to resulting array
         docsToInsert.push(docToInsert);
